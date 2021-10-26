6bea2e0c40683754daf15767f4e9854c341ec3f0
MehdiAtl <aitlallamehdi16@outlook.fr>
2021-10-26 22:11:38 +0200

    Changement du style du launcher

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow } = require('electron');
-
+const open = require("open")
 function createWindow () {
-    const win = new BrowserWindow({    width: 800,    height: 600  })
+    const win = new BrowserWindow({ 
+        width: 800,   
+        height: 600,
+        icon: "img/favicon.png" 
+    })
     win.loadFile('index.html')
 }
 app.on('window-all-closed', function () {  if (process.platform !== 'darwin') app.quit()})
